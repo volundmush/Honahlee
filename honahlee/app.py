b7dc887e46ff22358e52cefba72971461f483c98
Andrew Bastien <volundmush@gmail.com>
2020-06-09 04:40:43 -0700

    Getting this going.

---
+++ honahlee/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.8
+
+
+import asyncio
+import uvloop
+from tortoise import Tortoise
+
+
+async def main():
+    await Tortoise.init(
+        db_url="sqlite://db.sqlite3",
+        modules={'honahlee': ['honahlee.models']}
+    )
+
+    await Tortoise.generate_schemas(safe=True)
+    fake_app = None
+    from honahlee.networking.base import NetworkManager
+    from honahlee.networking.telnet import TelnetProtocol
+
+    net_man = NetworkManager(fake_app)
+    net_man.register_protocol('telnet', TelnetProtocol)
+    server = await net_man.create_server('telnet', '10.0.0.226', 4100, 'telnet', False)
+
+    await server.serve_forever()
+
+if __name__ == "__main__":
+    uvloop.install()
+    asyncio.run(main())
--- honahlee/launcher.py
+++ honahlee/launcher.py
@@ -1,30 +1,6 @@
 #!/usr/bin/env python3.8
 
-import asyncio
-import uvloop
-from tortoise import Tortoise
+import os
+import sys
+import argparse
 
-async def main():
-    print("HELLO WORLD!")
-    await Tortoise.init(
-        db_url="sqlite://db.sqlite3",
-        modules={'honahlee': ['honahlee.models']}
-    )
-    print("generating schemas...")
-    await Tortoise.generate_schemas(safe=True)
-    fake_app = None
-    from honahlee.networking.base import NetworkManager
-    from honahlee.networking.telnet import TelnetProtocol
-
-    net_man = NetworkManager(fake_app)
-    net_man.register_protocol('telnet', TelnetProtocol)
-    server = await net_man.create_server('telnet', '10.0.0.226', 4100, 'telnet', False)
-    print(server)
-
-    print("WE RUNNING?!")
-    await server.serve_forever()
-
-if __name__ == "__main__":
-    uvloop.install()
-    asyncio.run(main())
-    print("WE FINISHED?")
