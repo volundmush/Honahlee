b440a696a1b684052c89799637bb9de1c6c7b72b
Andrew Bastien <volundmush@gmail.com>
2020-06-11 04:37:45 -0700

    Getting this going.

--- honahlee/app.py
+++ honahlee/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.8
 
-import os, sys
+import os, sys, importlib
 import asyncio
 import uvloop
 from tortoise import Tortoise
@@ -8,12 +8,14 @@ from tortoise import Tortoise
 
 async def main():
 
-    await Tortoise.init(
-        db_url="sqlite://db.sqlite3",
-        modules={'honahlee': ['honahlee.models']}
-    )
+    try:
+        settings = importlib.import_module('gamedata.settings')
+    except Exception:
+        raise Exception("Could not import settings!")
 
+    await Tortoise.init(**settings.DATABASE_INIT)
     await Tortoise.generate_schemas(safe=True)
+
     fake_app = None
     from honahlee.networking.base import NetworkManager
     from honahlee.networking.telnet import TelnetProtocol
@@ -24,5 +26,11 @@ async def main():
 
 
 if __name__ == "__main__":
+    new_cwd = os.environ.get('HONAHLEE_TEMPLATE')
+    if not os.path.exists(new_cwd):
+        raise ValueError("Improper Honahlee profile!")
+    os.chdir(os.path.abspath(new_cwd))
+    sys.path.insert(0, os.getcwd())
+
     uvloop.install()
     asyncio.run(main())
