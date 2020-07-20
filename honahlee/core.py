fc06d88d7c17762f133fd1c65b0027c6d5b71fdd
Andrew Bastien <fubarp@gmail.com>
2020-07-20 19:03:47 -0400

    Beginning conversion to application framework.

--- honahlee/core.py
+++ honahlee/core.py
@@ -1,24 +1,103 @@
+from collections import defaultdict
+import uuid
 from honahlee.utils.misc import import_from_module
+
+
+class BaseConfig:
+
+    def __init__(self):
+        self.name = 'honahlee'
+        self.application = 'honahlee.core.Application'
+        self.classes = defaultdict(dict)
+        self.interfaces = dict()
+        self.tls = dict()
+        self.servers = defaultdict(dict)
+        self.clients = defaultdict(dict)
+
+    def setup(self):
+        self._config_classes()
+        self._config_interfaces()
+        self._config_tls()
+
+    def _config_classes(self):
+        """
+        Meant to add all necessary classes to the classes dictionary.
+        """
+        self.classes['services']['network'] = 'honahlee.services.network.ServerService'
+        self.classes['services']['client'] = 'honahlee.services.network.ClientService'
+        self.classes['servers']['base'] = 'honahlee.services.network.HonahleeServer'
+        self.classes['protocols']['telnet'] = 'honahlee.protocols.telnet.TelnetAsgiProtocol'
+        self.classes['consumers']['telnet'] = 'honahlee.protocols.telnet.AsyncTelnetConsumer'
+        self.classes['consumers']['game'] = 'honahlee.services.web.GameConsumer'
+        self.classes['consumers']['link'] = 'honahlee.services.web.LinkConsumer'
+        self.classes['consumers']['lifespan'] = 'honahlee.services.web.LifespanAsyncConsumer'
+
+    def _config_interfaces(self):
+        """
+        This lets you assign a name to an interface. 'internal' is for things hosted on an
+        loopback is for hosting on the host itself with no access from other computers.
+        internal is for your internal network. Same as loopback unless configured otherwise.
+        external is for any internet-facing adapters.
+        """
+        self.interfaces['loopback'] = "127.0.0.1"
+        self.interfaces['internal'] = "127.0.0.1"
+        self.interfaces['external'] = "0.0.0.0"
+
+    def _config_tls(self):
+        """
+        can have multiple contexts for different TLS/SSL cert combos.
+        These must be file paths to the certifications/keys in question.
+        """
+        self.tls['default'] = {
+            'cert': '',
+            'key': ''
+        }
+
+    def _config_servers(self):
+        pass
+
+    def _config_clients(self):
+        pass
 
 
 class BaseApplication:
 
-    def __init__(self, settings):
-        self.settings = settings
+    def __init__(self, config):
+        self.config = config
+        self.classes = defaultdict(dict)
         self.services = dict()
-        self.input_funcs = dict()
 
     async def setup(self):
-        for k, v in self.settings.APPLICATION_SERVICES.items():
-            service_class = import_from_module(v)
-            self.services[k] = service_class(self)
+        # Import all classes from the given config object.
+        for category, d in self.config.classes.items():
+            for name, path in d.items():
+                found = import_from_module(path)
+                found.app = self
+                self.classes[category][name] = found
 
-        for service in sorted(self.services.values(), key=lambda s: s.setup_order):
+        for name, v in sorted(self.classes['services'].items(), key=lambda x: getattr(x[1], 'init_order', 0)):
+            self.services[name] = v(self)
+
+        for service in sorted(self.services.values(), key=lambda s: getattr(s, 'load_order', 0)):
             await service.setup()
 
     async def start(self):
         for service in sorted(self.services.values(), key=lambda s: s.start_order):
             await service.start()
+
+    def fresh_uuid4(self, existing):
+        """
+        Given a list of UUID4s, generate a new one that's not already used.
+        Yes, I know this is silly. UUIDs are meant to be unique by sheer statistic unlikelihood of a conflict.
+        I'm just that afraid of collisions.
+        """
+        existing = set(existing)
+        fresh_uuid = None
+        while fresh_uuid is None:
+            new_uuid = uuid.uuid4()
+            if new_uuid not in existing:
+                fresh_uuid = new_uuid
+        return fresh_uuid
 
 
 class Application(BaseApplication):
@@ -26,11 +105,10 @@ class Application(BaseApplication):
 
 
 class BaseService:
+    app = None
+    init_order = 0
     setup_order = 0
     start_order = 0
-
-    def __init__(self, app):
-        self.app = app
 
     async def setup(self):
         pass
--- honahlee/services/network.py
+++ honahlee/services/network.py
@@ -1,10 +1,9 @@
 import asyncio
-import uuid
 from honahlee.core import BaseService
-from honahlee.utils.misc import import_from_module
 
 
-class HonahleeServer:
+class HonahleeNetworkBase:
+    app = None
 
     def __init__(self, service, name, address, port, protocol, tls):
         self.service = service
@@ -13,6 +12,12 @@ class HonahleeServer:
         self.port = port
         self.protocol = protocol
         self.tls = tls
+
+
+class HonahleeServer(HonahleeNetworkBase):
+
+    def __init__(self, service, name, address, port, protocol, tls):
+        super().__init__(service, name, address, port, protocol, tls)
         self.server = None
         self.task = None
 
@@ -32,62 +37,65 @@ class HonahleeServer:
             self.server.stop()
 
 
-class NetworkService(BaseService):
+class ServerService(BaseService):
     setup_order = -900
 
-    def __init__(self, app):
-        super().__init__(app)
-        self.games = dict()
-        self.server_classes = dict()
+    def __init__(self):
         self.servers = dict()
-        self.protocol_classes = dict()
         self.connections = dict()
-        self.ssl_context = None
-
-    def register_server_class(self, name, server_class):
-        self.server_classes[name] = server_class
-
-    def register_protocol_class(self, name, protocol_class):
-        self.protocol_classes[name] = protocol_class
 
     def create_server(self, name, address, port, server_class, protocol_class, tls):
         if name in self.servers:
             raise ValueError("That name conflicts with an existing server!")
-        if not (srv_class := self.server_classes.get(server_class, None)):
+        if not (srv_class := self.app.classes['servers'].get(server_class, None)):
             raise ValueError(f"Server Class {server_class} has not been registered!")
-        if not (prot_class := self.protocol_classes.get(protocol_class, None)):
+        if not (prot_class := self.app.classes['protocols'].get(protocol_class, None)):
             raise ValueError(f"Protocol Class {protocol_class} has not been registered!")
-        if tls and not self.ssl_context:
-            raise ValueError("TLS is not properly configured. Cannot start TLS server.")
-        new_server = srv_class(self, name, address, port, prot_class, tls)
+        tls_context = None
+        if tls and not (tls_context := self.app.tls.get(tls, None)):
+            raise ValueError(f"TLS is not properly configured. No context for {tls}! Cannot start TLS server.")
+        new_server = srv_class(self, name, address, port, prot_class, tls_context)
         self.servers[name] = new_server
-        return new_server
 
     def register_connection(self, conn):
-        fresh_uuid = None
-        while fresh_uuid is None:
-            new_uuid = uuid.uuid4()
-            if new_uuid not in self.connections:
-                fresh_uuid = new_uuid
-        conn.conn_id = fresh_uuid
+        new_uuid = self.app.fresh_uuid4(self.servers.keys())
+        conn.conn_id = new_uuid
         self.connections[conn.conn_id] = conn
 
     def unregister_connection(self, conn):
         del self.connections[conn.conn_id]
 
     async def setup(self):
-        for k, v in self.app.settings.SERVER_CLASSES.items():
-            srv_class = import_from_module(v)
-            self.register_server_class(k, srv_class)
-        for k, v in self.app.settings.PROTOCOL_CLASSES.items():
-            prot_class = import_from_module(v)
-            self.register_protocol_class(k, prot_class)
-        # do TLS init down here...
 
-        for k, v in self.app.settings.SERVERS.items():
-            self.create_server(k, self.app.settings.INTERFACE, v['port'], v['server_class'], v['protocol_class'],
+        for k, v in self.app.config.servers.items():
+            self.create_server(k, self.app.config.interfaces[v['interface']], v['port'], v['server_class'], v['protocol_class'],
                                v['tls'])
 
     async def start(self):
         for k, v in self.servers.items():
             await v.start()
+
+
+class HonahleeClient(HonahleeNetworkBase):
+
+    def __init__(self, service, name, address, port, protocol, tls):
+        super().__init__(service, name, address, port, protocol, tls)
+        self.client = None
+        self.task = None
+
+    async def accept(self, reader, writer):
+        pass
+
+    async def start(self):
+        pass
+
+    async def stop(self):
+        if self.client:
+            self.client.stop()
+
+
+class ClientService(BaseService):
+
+    def __init__(self):
+        self.clients = dict()
+        self.connections = dict()
